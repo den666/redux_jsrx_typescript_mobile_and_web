b3a9c90425f45a042a43f6d554944430f7c2028a
Dennis Espinoza <fev-ekd@MacBook-Pro-de-Dennis.local>
2018-01-12 12:40:30 -0500

    init config

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './scss/index.scss';
+import App from './js/App';
+
+export default ReactDOM.render(
+    <App/>,
+    document.getElementById('AppRoot')
+);
